306302cdd0f55e7b3d4ae54e33c55b3d12fd519c
Romane <romane71193@gmail.com>
2018-12-16 23:17:17 -0500

    menu cmp add

--- src/App.js
+++ src/App.js
@@ -1,27 +1,14 @@
 import React, { Component } from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
-import Menu from './components/MenuComponents';
-import { DISHES } from './shared/dishes';
-import logo from './logo.svg';
+import Main from './components/MainComponent';
 import './App.css';
 
 class App extends Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      dishes: DISHES
-    };
-  }
+  
   render() {
     return (
       <div className="App">
-         <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">Greens Gourmet</NavbarBrand>
-          </div>
-        </Navbar>
-        <Menu dishes={this.state.dishes} />
+                <Main />
       </div>
     );
   }
