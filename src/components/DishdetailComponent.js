3a648f33c42ca2513660506ae1d12202e2e7d48d
Romane <romane71193@gmail.com>
2018-12-19 14:16:25 -0500

    comments/leaders

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import { Card, CardImg, CardText, CardBody,
-    CardTitle } from 'reactstrap';
+    CardTitle,Breadcrumb, BreadcrumbItem  } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 
-    
+
         function RenderDish({dish}) {
             console.log('dish', dish)
             if (dish != null)
@@ -48,15 +49,27 @@ import { Card, CardImg, CardText, CardBody,
         return (
         <div className="container">
         <div className="row">
-            <div  className="col-12 col-md-5 m-1">
-            <RenderDish dish={props.dish}/>
+        <Breadcrumb>
+
+    <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+    <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+    </Breadcrumb>
+    <div className="col-12">
+    <h3>{props.dish.name}</h3>
+    <hr />
+    </div>                
+    </div>
+    <div className="row">
+    <div className="col-12 col-md-5 m-1">
+    <RenderDish dish={props.dish} />
             </div>
            
             <div  className="col-12 col-md-5 m-1">
-            <RenderComments comments={props.dish.comments}/>
+            <RenderComments comments={props.comments}/>
                 </div>
             </div>
-         </div>)
+         </div>
+         );
          else
          return(
              <div></div>
