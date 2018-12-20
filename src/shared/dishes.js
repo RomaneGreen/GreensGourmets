a4b8ac8b6efa904224457ea49829409c04c3b706
Romane <romane71193@gmail.com>
2018-12-19 20:33:06 -0500

    dishes refactor

--- src/shared/dishes.js
+++ src/shared/dishes.js
@@ -3,187 +3,44 @@ export const DISHES = [
                         {
                           id: 0,
                           name:'Whole Wheat Pizza',
-                          image: 'assets/images/wholewheatpizza.JPG',
+                          image: '/assets/images/wholewheatpizza.JPG',
                           category: 'mains',
                           label:'Hot',
                           price:'4.99',
-                          description:'Fresh baked whole wheat pizza',
-                          comments: [
-                            {
-                            id: 0,
-                            rating: 5,
-                            comment: "Imagine all the eatables, living in conFusion!",
-                            author: "John Lemon",
-                            date: "2012-10-16T17:57:28.556094Z"
-                            },
-                            {
-                            id: 1,
-                            rating: 4,
-                            comment: "Sends anyone to heaven, I wish I could get my mother-in-law to eat it!",
-                            author: "Paul McVites",
-                            date: "2014-09-05T17:57:28.556094Z"
-                            },
-                            {
-                            id: 2,
-                            rating: 3,
-                            comment: "Eat it, just eat it!",
-                            author: "Michael Jaikishan",
-                            date: "2015-02-13T17:57:28.556094Z"
-                            },
-                            {
-                            id: 3,
-                            rating: 4,
-                            comment: "Ultimate, Reaching for the stars!",
-                            author: "Ringo Starry",
-                            date: "2013-12-02T17:57:28.556094Z"
-                            },
-                            {
-                            id: 4,
-                            rating: 2,
-                            comment: "It's your birthday, we're gonna party!",
-                            author: "25 Cent",
-                            date: "2011-12-02T17:57:28.556094Z"
-                            }
-                        ]                        },
+                          featured: true,
+                          description:'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives'
+                        },
                        {
                           id: 1,
                           name:'Chickpea Stew',
-                          image: 'assets/images/chickpeastew.jpg',
+                          image: '/assets/images/chickpeastew.jpg',
                           category: 'appetizer',
                           label:'',
                           price:'1.99',
-                          description:'Organic vegetarian friendly chickpea stew',
-                          comments: [
-                            {
-                            id: 0,
-                            rating: 5,
-                            comment: "Imagine all the eatables, living in conFusion!",
-                            author: "John Lemon",
-                            date: "2012-10-16T17:57:28.556094Z"
-                            },
-                            {
-                            id: 1,
-                            rating: 4,
-                            comment: "Sends anyone to heaven, I wish I could get my mother-in-law to eat it!",
-                            author: "Paul McVites",
-                            date: "2014-09-05T17:57:28.556094Z"
-                            },
-                            {
-                            id: 2,
-                            rating: 3,
-                            comment: "Eat it, just eat it!",
-                            author: "Michael Jaikishan",
-                            date: "2015-02-13T17:57:28.556094Z"
-                            },
-                            {
-                            id: 3,
-                            rating: 4,
-                            comment: "Ultimate, Reaching for the stars!",
-                            author: "Ringo Starry",
-                            date: "2013-12-02T17:57:28.556094Z"
-                            },
-                            {
-                            id: 4,
-                            rating: 2,
-                            comment: "It's your birthday, we're gonna party!",
-                            author: "25 Cent",
-                            date: "2011-12-02T17:57:28.556094Z"
-                            }
-                        ]
-                        },
-                                   
+                          featured: false,
+                          description:'Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce'
+                          },        
                        {
                           id: 2,
                           name:' Apple Cinnamon Low carb donuts',
-                          image: 'assets/images/lowcarbdonuts.jpg',
+                          image: '/assets/images/lowcarbdonuts.jpg',
                           category: 'appetizer',
                           label:'New',
                           price:'1.99',
-                          description:'Keto friendly low carb apple cinnamon donuts',
-                          comments: [
-                            {
-                            id: 0,
-                            rating: 5,
-                            comment: "Imagine all the eatables, living in conFusion!",
-                            author: "John Lemon",
-                            date: "2012-10-16T17:57:28.556094Z"
-                            },
-                            {
-                            id: 1,
-                            rating: 4,
-                            comment: "Sends anyone to heaven, I wish I could get my mother-in-law to eat it!",
-                            author: "Paul McVites",
-                            date: "2014-09-05T17:57:28.556094Z"
-                            },
-                            {
-                            id: 2,
-                            rating: 3,
-                            comment: "Eat it, just eat it!",
-                            author: "Michael Jaikishan",
-                            date: "2015-02-13T17:57:28.556094Z"
-                            },
-                            {
-                            id: 3,
-                            rating: 4,
-                            comment: "Ultimate, Reaching for the stars!",
-                            author: "Ringo Starry",
-                            date: "2013-12-02T17:57:28.556094Z"
-                            },
-                            {
-                            id: 4,
-                            rating: 2,
-                            comment: "It's your birthday, we're gonna party!",
-                            author: "25 Cent",
-                            date: "2011-12-02T17:57:28.556094Z"
-                            }
-                        ]
+                          featured: false,
+                          description:'A quintessential ConFusion experience, is it a vada or is it a donut?'
                         },                       
                        {
                           id: 3,
                           name:'Chocolate Vegan Low carb Cheesecake',
-                          image: 'assets/images/cheesecake.jpeg',
+                          image: '/assets/images/cheesecake.jpeg',
                           category: 'dessert',
                           label:'',
                           price:'2.99',
-                          description:'Dairy,gluten and soy free' ,
-                          comments: [
-                            {
-                            id: 0,
-                            rating: 5,
-                            comment: "Imagine all the eatables, living in conFusion!",
-                            author: "John Lemon",
-                            date: "2012-10-16T17:57:28.556094Z"
-                            },
-                            {
-                            id: 1,
-                            rating: 4,
-                            comment: "Sends anyone to heaven, I wish I could get my mother-in-law to eat it!",
-                            author: "Paul McVites",
-                            date: "2014-09-05T17:57:28.556094Z"
-                            },
-                            {
-                            id: 2,
-                            rating: 3,
-                            comment: "Eat it, just eat it!",
-                            author: "Michael Jaikishan",
-                            date: "2015-02-13T17:57:28.556094Z"
-                            },
-                            {
-                            id: 3,
-                            rating: 4,
-                            comment: "Ultimate, Reaching for the stars!",
-                            author: "Ringo Starry",
-                            date: "2013-12-02T17:57:28.556094Z"
-                            },
-                            {
-                            id: 4,
-                            rating: 2,
-                            comment: "It's your birthday, we're gonna party!",
-                            author: "25 Cent",
-                            date: "2011-12-02T17:57:28.556094Z"
-                            }
-                        ]
-                        }
-                    ]; 
+                          featured: false,
+        description:'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms'
+        }
+    
+  ]; 
                         
                 
