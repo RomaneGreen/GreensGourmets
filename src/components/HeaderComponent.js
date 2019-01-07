213cc95b3338189d734004a59bacc70805ba1f95
Romane G <romane71193@gmail.com>
2019-01-06 21:23:50 -0500

    netlify+cd

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -56,7 +56,7 @@ class Header extends Component {
                             <Nav className="ml-auto" navbar>
                                 <NavItem>
                                     {/* <Button outline onClick={this.toggleModal}><span className="fa fa-sign-in fa-lg"></span> Login</Button> */}
-                                    <li><a href="https://www.greensgourments.com">Online Store!</a></li>
+                                    <li><a href="https://www.greensgourments.com">Greens's Gourmets Online!</a></li>
                                 </NavItem>
                             </Nav>
                         </Collapse>
